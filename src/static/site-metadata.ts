971bff1340d83f0296d0f6af04c61a9dd9088367
xuezhilong <xuezhilong@huizhuyun.com>
2025-06-18 16:43:07 +0800

    init

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,22 +16,22 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'running.xiaolongge.online',
+  logo: 'https://avatars.githubusercontent.com/u/30064467?v=4',
   description: 'Personal site and blog',
   navLinks: [
-    {
-      name: 'Summary',
-      url: `${getBasePath()}/summary`,
-    },
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
+    // {
+    //   name: 'Summary',
+    //   url: `${getBasePath()}/summary`,
+    // },
+    // {
+    //   name: 'Blog',
+    //   url: 'https://github.com/yihong0618/gitblog',
+    // },
+    // {
+    //   name: 'About',
+    //   url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+    // },
   ],
 };
 
